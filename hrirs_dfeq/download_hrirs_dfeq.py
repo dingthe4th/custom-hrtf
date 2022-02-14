0a5bb52a2e73d2f3adf91de6e747d05640603de9
dingkun <46555394+rglbiv@users.noreply.github.com>
2022-02-14 18:00:12 +0800

    Added hrirs folders
    Run .py script in each folder to download the .sofa files from https://sofacoustics.org/data/database/3d3a/

---
+++ hrirs_dfeq/download_hrirs_dfeq.py
@@ -0,0 +1,20 @@
+# Download HRTFS from https://sofacoustics.org/data/database/3d3a/
+import requests
+i = 0 # Index
+# Default
+hrir_def = '_HRIRs.sofa'
+# Diffuse-field equalization
+hrir_dfeq = '_HRIRs_dfeq.sofa'
+# Low-frequency extension
+hrir_lfc = '_HRIRs_lfc.sofa'
+
+for x in range(42):
+	i += 1
+	name = 'Subject'
+	name = name + str(i) + hrir_dfeq # Change the hrir_dfeq with what you want
+
+	url = 'https://sofacoustics.org/data/database/3d3a/'+ name
+	print('Downloading:   ' + url)
+
+	r = requests.get(url, allow_redirects=True)
+	open(name, 'wb').write(r.content)
